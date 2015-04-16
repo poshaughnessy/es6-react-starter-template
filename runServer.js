a51558a4565f2dc31517a70602acc4d02ce1608a
Peter O'Shaughnessy <peter.oshaughnessy@pearson.com>
2015-04-16 11:26:27 +0100

    Add comment about server-side transpilation

--- runServer.js
+++ runServer.js
@@ -1,8 +1,15 @@
 /**
-* To run the server on the command line.
+* To run the server on the command line or on your hosting provider.
 */
 
-// Enable ES6 - this will make it automatically transpile required files. See: http://babeljs.io/docs/usage/require/
+/**
+ * Enable ES6 - this will make it automatically transpile required files. See: http://babeljs.io/docs/usage/require/
+ * I've left this as runtime transpilation for now just for simplicity, but a real app should perform this as a build
+ * step for performance.
+ *
+ * NB. Heroku supports some ES6 language features through the --harmony flag, but only a subset. See:
+ * https://devcenter.heroku.com/articles/node-best-practices#try-new-things
+ */
 require('babel/register');
 
 var server = require('./server');
