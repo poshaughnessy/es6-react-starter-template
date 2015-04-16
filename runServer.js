4d8dfa595636684ec55cd193dbc9414b7180a7bb
Peter O'Shaughnessy <peter.oshaughnessy@pearson.com>
2015-04-16 12:09:29 +0100

    Update comment about server-side ES6

--- runServer.js
+++ runServer.js
@@ -5,10 +5,9 @@
 /**
  * Enable ES6 - this will make it automatically transpile required files. See: http://babeljs.io/docs/usage/require/
  * I've left this as runtime transpilation for now just for simplicity, but a real app should perform this as a build
- * step for performance.
- *
- * NB. Heroku supports some ES6 language features through the --harmony flag, but only a subset. See:
- * https://devcenter.heroku.com/articles/node-best-practices#try-new-things
+ * step for performance - or ideally use native support. My newer template uses native support in io.js, which is
+ * ahead of node.js for ES6:
+ * https://github.com/poshaughnessy/es6-react-jspm-starter-template
  */
 require('babel/register');
 
